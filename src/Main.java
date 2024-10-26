89b2cc84cada03582c0260b7c7c3eb9fadef9140
giorgi <giorgichqareuli712@gmail.com>
2024-10-26 18:43:50 +0400

    commit

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+public class Main {
+    public static void main(String[] args) {
+//        int a = 3;
+//        int b = 78;
+//        int c = 24;
+//        double average = (a + b + c) / 3.0;
+//        System.out.println("სამი რიცხვის საშუალო: " + average);
+
+
+
+
+//        int number = 658;
+//        if (number % 2 == 0) {
+//            System.out.println(number + " ლუწი რიცხვია");
+//        } else {
+//            System.out.println(number + " კენტი რიცხვია");
+//        }
+
+
+
+
+//        int num1 = 16;
+//        int num2 = 24;
+//        if (num1 > num2) {
+//            System.out.println("უდიდესი რიცხვია: " + num1);
+//        } else if (num2 > num1) {
+//            System.out.println("უდიდესი რიცხვია: " + num2);
+//        } else {
+//            System.out.println("ორივე რიცხვი ტოლია: " + num1);
+//        }
+
+
+
+
+//        int number = 1111;
+//
+//        int thousands = number / 1000;
+//        int hundreds = (number / 100) % 10;
+//        int tens = (number / 10) % 10;
+//        int units = number % 10;
+//
+//        int sum = thousands + hundreds + tens + units;
+//
+//        System.out.println("ციფრთა ჯამია: " + sum);
+
+
+
+
+//        int kutxe1 = 55;
+//        int kutxe2 = 80;
+//        int kutxe3 = 42;
+//
+//        if (kutxe1 + kutxe2 > kutxe3 && kutxe1 + kutxe3 > kutxe2 && kutxe2 + kutxe3 > kutxe1) {
+//            System.out.println("სამკუთხედი შედგება.");
+//        } else {
+//            System.out.println("სამკუთხედი ვერ შედგება.");
+//        }
+    }
+}
